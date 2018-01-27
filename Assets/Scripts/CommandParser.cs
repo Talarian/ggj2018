fbb05f24846c68dd43268119c83df78d276d0aa3
Joey Wiggs <jswiggs@flyinghelmetgames.com>
2018-01-27 11:31:35 -0800

    COmmand Parser basics + metas

---
+++ Assets/Scripts/CommandParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandParser : MonoBehaviour
+{
+	private char[] charSeparators = { ' ' };
+
+	public string ParseCommand(string command)
+	{
+		string[] result;
+		result = command.Split(charSeparators);
+
+
+		return "";
+	}
+}
