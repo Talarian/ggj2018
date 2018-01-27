26bf71c4de042df808be6b4b8759b36164c75da5
Scott Penner <spenner@flyinghelmetgames.com>
2018-01-27 11:44:22 -0800

    Lots of Art stuff. Integrating Rover into main scene.

---
+++ Assets/Scripts/cameraOrbitLock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraOrbitLock : MonoBehaviour {
+
+    private Quaternion initialRotation = Quaternion.identity;
+    public bool lockX = false;
+    public bool lockY = false;
+    public bool lockZ = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        initialRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = lockX == true ? initialRotation.eulerAngles.x : transform.rotation.eulerAngles.x;
+        float y = lockY == true ? initialRotation.eulerAngles.y : transform.rotation.eulerAngles.y;
+        float z = lockY == true ? initialRotation.eulerAngles.z : transform.rotation.eulerAngles.z;
+	}
+}
