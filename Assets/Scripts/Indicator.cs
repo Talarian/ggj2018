7d3211870f4817713985d9af443084ff350fdfec
Scott Penner <spenner@flyinghelmetgames.com>
2018-01-28 12:58:54 -0800

    various

--- Assets/Scripts/Indicator.cs
+++ Assets/Scripts/Indicator.cs
@@ -6,9 +6,42 @@ public class Indicator : MonoBehaviour
 {
     public GameObject normal;
     public GameObject highlight;
+    public bool flashOnStart = false;
+
+    void Awake ()
+    {
+        if(flashOnStart)
+        {
+            DoFlashing();
+        }
+    }
 
     public virtual void DoFlashing()
     {
-
+        StartCoroutine(Flash());
     }
+
+    IEnumerator Flash()
+    {
+        normal.SetActive(false);
+        highlight.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        normal.SetActive(true);
+        highlight.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        normal.SetActive(false);
+        highlight.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        normal.SetActive(true);
+        highlight.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        normal.SetActive(false);
+        highlight.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        normal.SetActive(true);
+        highlight.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+    }
+
+
 }
