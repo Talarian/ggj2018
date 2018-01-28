7a023c94cae45469cc34f5b6d3c279386b662fec
Scott Penner <spenner@flyinghelmetgames.com>
2018-01-27 22:54:56 -0800

    mission control scene, audio

---
+++ Assets/Scripts/AudioInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioInput : MonoBehaviour
+{
+    private AudioSource source;
+
+    public AudioClip[] keySounds;
+    public AudioClip[] positiveSounds;
+    public AudioClip[] negativeSounds;
+    public AudioClip[] feedbackSounds;
+
+	// Use this for initialization
+	void Start ()
+    {
+        source = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.anyKeyDown)
+        {
+            source.PlayOneShot(keySounds[Random.Range(0, keySounds.Length - 1)]);
+        }
+	}
+
+    public void PlaySoundPositive()
+    {
+        source.PlayOneShot(negativeSounds[Random.Range(0, negativeSounds.Length - 1)]);
+    }
+
+    public void PlaySoundNegative()
+    {
+        source.PlayOneShot(feedbackSounds[Random.Range(0, feedbackSounds.Length - 1)]);
+    }
+
+    public void PlaySoundFeedback()
+    {
+        source.PlayOneShot(feedbackSounds[Random.Range(0, feedbackSounds.Length - 1)]);
+    }
+
+}
