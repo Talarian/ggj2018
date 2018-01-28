6125b80c637c14b76ede0cf111b7a7d2644a9426
3luke33 <3shadow33>
2018-01-27 21:36:25 -0800

    Compass needle rotation code

---
+++ Assets/Needle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needle : MonoBehaviour {
+
+    public Transform rover;
+    RectTransform rectTransform;
+
+    void Start() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rectTransform.rotation = Quaternion.Euler(0,0, rover.transform.rotation.eulerAngles.y);
+	}
+}
