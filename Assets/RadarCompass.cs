7021c2d9c6e5383ca77c77c1387dfb21c8dea6ad
3luke33 <3shadow33>
2018-01-27 22:30:17 -0800

    Radar compass code start

---
+++ Assets/RadarCompass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarCompass : MonoBehaviour {
+
+    [System.Serializable]
+    public class Configuration {
+        public BasicRadar radar;
+    }
+
+    public Configuration configuration;
+    public RectTransform arrow;
+
+    private void Start() {
+        Debug.Assert(arrow != null, "Heightmap requires a text mesh pro textbox");
+        Debug.Assert(configuration.radar != null, "HeightmapDisplay requires a heightmap sensor");
+
+        configuration.radar.OnSensorDataAvailable += Radar_OnSensorDataAvailable;
+
+    }
+
+    private void Radar_OnSensorDataAvailable(SensorData obj) {
+        RadarData data = obj as RadarData;
+        Debug.Assert(data != null);
+
+        arrow.rotation = data.compassDirection;
+    }
+}
