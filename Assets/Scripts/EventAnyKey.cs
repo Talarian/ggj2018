c192e657ade35a8fdbdbccfdfaeef9a1eb7e4624
Scott Penner <spenner@flyinghelmetgames.com>
2018-01-28 04:31:12 -0800

    Main scene UI updates

---
+++ Assets/Scripts/EventAnyKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventAnyKey : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject newSelected;
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.anyKey)
+        {
+            eventSystem.SetSelectedGameObject(newSelected);
+            gameObject.SetActive(false);
+        }
+	}
+}
