113e254da1ac6bd27958284c1a5ae6987ad55eaf
Joey Wiggs <jswiggs@flyinghelmetgames.com>
2018-01-27 22:32:44 -0800

    Download Queue UI

---
+++ Assets/Scripts/UI/CommandQueueTextboxDownload.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class CommandQueueTextboxDownload : CommandQueueTextBox
+{
+	protected override void Start()
+	{
+		base.Start();
+
+		commandQueue.OnCommandDequeued += CommandQueue_OnCommandDequeued;
+	}
+
+	private void CommandQueue_OnCommandDequeued(Command obj)
+	{
+		if (obj.commandType == Command.CommandType.Sensors)
+		{
+			Command commandCopy = new Command();
+			commandCopy.commandType = obj.commandType;
+			commandCopy.commandValue = obj.commandValue;
+			commandCopy.originalText = obj.originalText;
+			commandCopy.timeStamp = DateTime.Now;
+			waitingCommands.Add(commandCopy);
+		}
+	}
+}
+
