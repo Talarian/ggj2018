70e43b8dafd39bba3d09ddb8d72ed4ab8e882aff
Scott Penner <spenner@flyinghelmetgames.com>
2018-01-27 23:41:50 -0800

    main scene now integrates the command level

---
+++ Assets/Scripts/MainSceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneLoader : MonoBehaviour
+{
+    public string sceneToLoad;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
+    }
+}
