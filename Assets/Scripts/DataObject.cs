18a5b6b5231f7e79dd869e82cfe8341b10d2c7ee
Scott Penner <spenner@flyinghelmetgames.com>
2018-01-28 01:26:09 -0800

    main scene updates

---
+++ Assets/Scripts/DataObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DataObject : Indicator
+{
+    public string dataValue;
+    public TextMeshProUGUI valueNormal;
+    public TextMeshProUGUI valueHighlight;
+
+    void Start()
+    {
+        normal = valueNormal.transform.parent.gameObject;
+        normal.SetActive(true);
+        highlight = valueHighlight.transform.parent.gameObject;
+        highlight.SetActive(false);
+    }
+
+    public void ChangeValue(string newValue)
+    {
+        valueNormal.text = newValue;
+        valueHighlight.text = newValue;
+        DoFlashing();
+    }
+}
---
+++ Assets/Scripts/Indicator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicator : MonoBehaviour
+{
+    public GameObject normal;
+    public GameObject highlight;
+
+    public virtual void DoFlashing()
+    {
+
+    }
+}
