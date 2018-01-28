7ee5fd8125dbb0efaf894a1912830676dd4f1e7b
Scott Penner <spenner@flyinghelmetgames.com>
2018-01-27 20:39:43 -0800

    mission control scene

---
+++ Assets/Scripts/cameraCommandFocus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraCommandFocus : MonoBehaviour
+{
+    private Animator animator;
+    public float currentFocus;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if(Input.mousePosition.x < Screen.width / 0.5f)
+        //{
+        //    animator.SetFloat("focusOnMonitor", Input.mousePosition.x / Screen.width, 0.25f, Time.deltaTime);
+        //}
+
+        animator.SetFloat("focusOnMonitor", Input.mousePosition.x / Screen.width, 0.25f, Time.deltaTime);
+
+
+    }
+}
