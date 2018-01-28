977dd2e05da88caf2bb638972f1d12c374a02572
Joey Wiggs <jswiggs@flyinghelmetgames.com>
2018-01-27 23:20:09 -0800

    Display Pings for Radar

--- Assets/RadarCompass.cs
+++ Assets/RadarCompass.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,14 @@ public class RadarCompass : MonoBehaviour {
     [System.Serializable]
     public class Configuration {
         public BasicRadar radar;
+		public GameObject pingPrefab;
+		public float radarRadius;
     }
 
     public Configuration configuration;
     public RectTransform arrow;
+
+	private List<GameObject> pings = new List<GameObject>();
 
     private void Start() {
         Debug.Assert(arrow != null, "Heightmap requires a text mesh pro textbox");
@@ -20,10 +25,49 @@ public class RadarCompass : MonoBehaviour {
 
     }
 
-    private void Radar_OnSensorDataAvailable(SensorData obj) {
-        RadarData data = obj as RadarData;
-        Debug.Assert(data != null);
+    private void Radar_OnSensorDataAvailable(SensorData obj)
+	{
+		RadarData data = obj as RadarData;
+		Debug.Assert(data != null);
 
-        arrow.rotation = data.compassDirection;
-    }
+		arrow.rotation = data.compassDirection;
+		ClearPings();
+
+		foreach (Vector3 ping in data.pings)
+		{
+			DisplayPing(ping);
+		}
+	}
+
+	private void ClearPings()
+	{
+		while (pings.Count > 0)
+		{
+			DestroyImmediate(pings[0]);
+			pings.RemoveAt(0);
+		}
+	}
+
+	private void DisplayPing(Vector3 ping)
+	{
+		GameObject obj = Instantiate(configuration.pingPrefab);
+		RectTransform rectTransform = obj.GetComponent<RectTransform>();
+		rectTransform.SetParent(this.transform);
+		pings.Add(obj);
+
+		ping.y = 0f;
+		ping = (ping / 50f) * configuration.radarRadius;
+
+		// Convert to UI space
+		ping.y = ping.z;
+		ping.z = 0f;
+
+		if (ping.magnitude > configuration.radarRadius)
+		{
+			ping.Normalize();
+			ping = ping * configuration.radarRadius;
+		}
+
+		rectTransform.localPosition = ping;
+	}
 }
